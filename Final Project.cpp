258edcd42fd67ea2b82ebf7c0b766319a98606c3
sefira karina <sefira karina>
2016-10-28 12:33:42 +0700

    push

--- Final Project.cpp
+++ Final Project.cpp
@@ -2,54 +2,35 @@
 #include <string>
 using namespace std;
 
+//PS : I've made some addition in the docx file
 
-int main (){
-	
-	int num = 50; // max input
-	float score[num],total = 0,average,remedyScore[num],retestScore[num];
-	string name[num];
-	int students,tests ;
-	const int minScore=75 ;
-	
-	
-	cout << "Enter number of students = ";
-	cin >> students ;
-	
-	cout << "Enter number of tests given = ";
-	cin >> tests ;
-	cout <<endl;
-	
-	
-for (int i=0 ; i<students ; i++) // to know students's names
+const int minScore=75 ;
+
+void studentsName(int students,string name[])
 {
-	cout << "Enter student "<<i+1<<"'s name = " ;
-	cin >> name[i] ;
-}
+	for (int i=0 ; i<students ; i++) // to know students's names
+	{
+		cout << "Enter student "<<i+1<<"'s name = " ;
+		cin >> name[i] ;
+	}
 	
 	cout<<endl ;
-	
-//===============================================================//
+}
 
-for (int i=0 ; i<tests ; i++)
-{	
-	cout << "TEST "<< i+1 <<endl <<"--------"<<endl ;
+/////////////////////////////////////////////////////////////////////////////////
 
-
-	for (int i=0 ; i<students ; i++) // to know student's tests scores
+void calcScores(int students,string name[],float score[],float total,float average,float retestScore[],float remedyScore[])
+{
+		for (int i=0 ; i<students ; i++) // to know student's tests scores
 	{
 		cout << "input "<<name[i]<<"'s test score "  << " = ";
 		cin>>score[i];
 		
 		total = total + score[i] ;
 	}
-
-//=================================================================================//
-	
-	cout<<endl<<"---------------------------------------------------"<<endl;
 	
 	average = total / students;
-
-//================================================================================//
+	cout<<"average score = "<< average<<endl<<endl;
 	
 	if(average < minScore) 
 	{
@@ -80,15 +61,19 @@ for (int i=0 ; i<tests ; i++)
 			{
 				cout << "Please input " << name[i] << "'s remedy score =";
 				cin >>remedyScore[i];
-				
-				if (remedyScore[i]>minScore)
-				remedyScore[i] = minScore ;
-				
-				else if(remedyScore[i]<score[i])
+
+				if(remedyScore[i]<score[i])
 				remedyScore[i]=score[i];
 				
 				else if(remedyScore[i]>score[i])
-				score[i]=remedyScore[i];
+				{
+					if(remedyScore[i]<minScore)
+					score[i]=remedyScore[i];
+					
+					else if (remedyScore[i]>minScore)
+					remedyScore[i] = minScore ;	
+					score[i]=remedyScore[i];
+				}
 				
 				remedyScore[i]=score[i];
 			}
@@ -96,10 +81,10 @@ for (int i=0 ; i<tests ; i++)
 		}
 		cout<<endl<<endl;
 	}
-	
-	
-	cout << "Final score for test "<< i+1 <<" are : "<<endl;
-	
+}
+///////////////////////////////////////////////////////////////////////////////////
+void testResults(int students,string name[],float score[])
+{
 	for (int i=0 ; i<students ; i++) 
 		{
 			cout << name [i] << " : "<<score[i]<<endl;
@@ -108,6 +93,44 @@ for (int i=0 ; i<tests ; i++)
 		
 	cout<<endl;
 }
+
+
+
+///////////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+	
+	int num = 50; 
+	float score[num],total = 0,average,remedyScore[num],retestScore[num];
+	string name[num];
+	int students,tests;
+	const int minScore=75 ;
+	
+	cout << "Enter number of students = ";
+	cin >> students ;
+	
+	cout << "Enter number of tests given = ";
+	cin >> tests ;
+	cout <<endl;
+	
+	studentsName(students,name);
+
+	for (int i=0 ; i<tests ; i++)
+	{
+		cout << "TEST "<< i+1 <<endl <<"--------"<<endl ;
+		
+		calcScores(students,name,score,total,average,retestScore,remedyScore);
+		
+		cout << "Final score for test "<< i+1 <<" are : "<<endl;
+		
+		testResults(students,name,score);
+		
+	//	for (int i=0 ; i<students ; i++) 
+	//	{
+	//		cout << name [i] << " : "<< (score[i]+score[i])/tests) <<endl ; 
+	//	}
+	}
 	
 	
 	return 0;
