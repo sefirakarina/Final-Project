80f48585e8038533b4dd71964d7043b16eef4715
sefira karina <sefira karina>
2016-11-06 22:47:50 +0700

    final push

--- final final/finalproject.hpp
+++ final final/finalproject.hpp
@@ -1,3 +1,8 @@
+//  finalproject.hpp
+// Final Project
+//  Sefira Karina-2001586155-Batch 2020
+//  Copyright © 2016 Sefira Karina. All rights reserved.
+
 #ifndef finalproject_hpp
 #define finalproject_hpp
 #include<iostream>
@@ -17,6 +22,7 @@ class Data
 	int getStudents();
 	string getName();
 	
+	void rules();
 	void calcScores(int,string name[],float score[],float total,float average,float retestScore[],float remedyScore[]);
 	void calcScores2(int,string name[],float score2[],float total2,float average2,float retestScore2[],float remedyScore2[]);
 	void countAv(float score[],float score2[],float mean[],string name[]);
@@ -24,7 +30,7 @@ class Data
 	void reportScore(int,float finalMidScore[],float mean[],float midScore[],string name[]);
 };
 
-Data::Data()
+Data::Data() //constructor
 {
 	students=students ;
 		
@@ -32,14 +38,36 @@ Data::Data()
 
 ///////////////////////////////////////////////////////////////////////////////////
 
-void  Data::setStudents(int humans)
+void  Data::setStudents(int humans) 
+//mutator to set the amount of students into the user input
 {
 	students=humans ;
 }
 
 /////////////////////////////////////////////////////////////////////////////////
 
+void Data::rules() //the rules of scoring 
+{
+	cout <<"RULES OF SCORING FOR MID REPORT : \n"
+		<<"-------------------------------------\n\n"
+		<<"1.There will be 2 tests and 1 mid test\n\n"
+		<<"2.The minimum score is 75\n\n"
+		<<"3.If the average score of the test is bellow that,retest\n"
+		<<"  will be needed,the max retest score is 100\n\n"
+		<<"4.If a student get red score but no retest,the student\n"
+		<<"  will undergo a remedy,with the max score of 75\n"
+		<<"  if the remedy score is above 75,the score will\n"
+		<<"  be count as 75,if the remedy score is bellow 75,\n"
+		<<"  the highest score will be taken (ex:score=70 ,remedy\n" 
+		<<"  score =60, then 70 will be taken)\n\n"
+		<<"5.There will be no remedy for the mid test\n\n"
+		<<"6.The final score for the mid report card is 30% of the\n"
+		<<"  average test score + 70% of the mid score\n"
+		<<"------------------------------------------------------------\n\n";
+}
+
 void Data::setName(string)
+//mutator to set student's name 
 {
 	string namee[20];
 		
@@ -52,6 +80,7 @@ void Data::setName(string)
 /////////////////////////////////////////////////////////////////////////////////
 
 int Data::getStudents()
+//accessor to return the value of student
 {
 	return students ;
 }
@@ -59,6 +88,7 @@ int Data::getStudents()
 //////////////////////////////////////////////////////////////////////////////////
 
 string Data::getName()
+//accessor to return the value of student's names
 {
 	return name[20];
 }
@@ -66,50 +96,65 @@ string Data::getName()
 ////////////////////////////////////////////////////////////////////////////////////
 
 void Data::calcScores(int,string name[],float score[],float total,float average,float retestScore[],float remedyScore[])
+/*the function void calcScores2 and void midTerm 
+have the exact same explanation as void calcScore , 
+the only difference is void midTerm doesn't have remedy*/
+
 {
 	const int minScore=75;
 	
-		for (int i=0 ; i<students ; i++) // to know student's tests scores
+		for (int i=0 ; i<students ; i++) 
+		// to know student's tests scores
 	{
 		cout << "input "<<name[i]<<"'s test score "  << " = ";
 		cin>>score[i];
 		
 		while(score[i]>100 || score[i]<0)
+		/*input validation , if the score is bellow 0 or above 100, 
+		the program will ask the user to reinput*/
 		{
 			cout << "input "<<name[i]<<"'s test score (0-100) "  << " = ";
 			cin>>score[i];
 		}
 		
-		total = total + score[i] ;
+		total = total + score[i] ; 
+		// count the total score of all students 
+		//(used to count average score later)
 	}
 	
-	average = total / students;
-	cout<<"average score = "<< average<<endl<<endl;
+	average = total / students; 
+	// count the average score for the test
+	cout<<endl<<"average score = "<< average<<endl<<endl;
 	
 	if(average < minScore) 
+	//if the average above is bellow 75,a retest will be held
 	{
-		cout<<"The average score is bellow the minimum score (75)"<<endl<<"please input students's retest scores"<<endl<<endl;
+		cout<<"The average score is bellow the minimum score (75)"<<endl
+			<<"please input students's retest scores"<<endl<<endl;
 		
 			for (int i=0 ; i<students ; i++) 
 			{
-				cout << "input "<<name[i]<<"'s retest's score = ";
-				cin>>retestScore[i];
 				
-				while(retestScore[i]>100 || retestScore[i]<0)
+				do 
+				/*input validation , if the score is bellow 0 or above 100, 
+				the program will ask the user to reinput*/
 				{
 					cout << "input "<<name[i]<<"'s retest's score (0-100) = ";
 					cin>>retestScore[i];
 				}
 				
-				if(retestScore[i]<score[i])
+				while(retestScore[i]>100 || retestScore[i]<0);
+				
+				
+				if(retestScore[i]<score[i]) 
+				/*the highest score between the retest score
+				 and the previous test score will be used*/
 				retestScore[i]=score[i];
 				
 				else if(retestScore[i]>score[i])
 				score[i]=retestScore[i];
 				
 				retestScore[i]=score[i];
-			
-		
 			}
 		cout<<endl<<endl;	
 	}
@@ -120,25 +165,32 @@ void Data::calcScores(int,string name[],float score[],float total,float average,
 		{
 			if (score[i]< minScore)
 			{
-				cout << "Please input " << name[i] << "'s remedy score =";
-				cin >>remedyScore[i];
 				
-				while(remedyScore[i]>100 || remedyScore[i]<0)
+				do /*input validation , if the score is bellow 0 or above 100, 
+					the program will ask the user to reinput*/
 				{
-					cout << "Please input " << name[i] << "'s remedy score (0-100) =";
-					cin >>remedyScore[i];
+					cout << "input "<<name[i]<<"'s remedy's score (0-100) = ";
+					cin>>remedyScore[i];
 				}
+				
+				while(remedyScore[i]>100 || remedyScore[i]<0);
 
 				if(remedyScore[i]<score[i])
 				remedyScore[i]=score[i];
+				/*if the remedy score is less than previous score,
+				the highest score will be taken*/
 				
 				else if(remedyScore[i]>score[i])
 				{
 					if(remedyScore[i]<minScore)
 					score[i]=remedyScore[i];
+					/*if the remedy score is above 75, 
+					the score will be count as 75*/
 					
 					else if (remedyScore[i]>minScore)
 					remedyScore[i] = minScore ;	
+					/*if the remedy score is above 75, 
+					it will count as 75*/
 					score[i]=remedyScore[i];
 				}
 				
@@ -153,16 +205,16 @@ void Data::calcScores(int,string name[],float score[],float total,float average,
 	cout<<"final score for this test : "<<endl;
 	
 	for (int i=0 ; i<students ; i++) 
+	//to show all scores after remedy/retest
 		{
 			cout << name [i] << " : "<<score[i]<<endl;
-		
 		}
 		
 	cout<<endl;
 }
 
 //=================================================================================================================================//
-	
+
 void Data::calcScores2(int,string name[],float score2[],float total2,float average2,float retestScore2[],float remedyScore2[])
 {
 	const int minScore=75;
@@ -182,22 +234,23 @@ void Data::calcScores2(int,string name[],float score2[],float total2,float avera
 	}
 	
 	average2 = total2 / students;
-	cout<<"average score = "<< average2<<endl<<endl;
+	cout<<endl<<"average score = "<< average2<<endl<<endl;
 	
 	if(average2 < minScore) 
 	{
-		cout<<"The average score is bellow the minimum score (75)"<<endl<<"please input students's retest scores"<<endl<<endl;
+		cout<<"The average score is bellow the minimum score (75)"<<endl
+			<<"please input students's retest scores"<<endl<<endl;
 		
 			for (int i=0 ; i<students ; i++) 
 			{
-				cout << "input "<<name[i]<<"'s retest's score = ";
-				cin>>retestScore2[i];
 				
-				while(retestScore2[i]>100 || retestScore2[i]<0)
+				do
 				{
 					cout << "input "<<name[i]<<"'s retest's score (0-100) = ";
 					cin>>retestScore2[i];
 				}
+				
+				while(retestScore2[i]>100 || retestScore2[i]<0);
 				
 				if(retestScore2[i]<score2[i])
 				retestScore2[i]=score2[i];
@@ -217,14 +270,13 @@ void Data::calcScores2(int,string name[],float score2[],float total2,float avera
 		{
 			if (score2[i]< minScore)
 			{
-				cout << "Please input " << name[i] << "'s remedy score =";
-				cin >>remedyScore2[i];
-				
-				while(remedyScore2[i]>100 || remedyScore2[i]<0)
+				do
 				{
-					cout << "Please input " << name[i] << "'s remedy score (0-100) =";
+					cout << "Please input " << name[i] << "'s remedy score =";
 					cin >>remedyScore2[i];
 				}
+				
+				while(remedyScore2[i]>100 || remedyScore2[i]<0);
 
 				if(remedyScore2[i]<score2[i])
 				remedyScore2[i]=score2[i];
@@ -240,7 +292,6 @@ void Data::calcScores2(int,string name[],float score2[],float total2,float avera
 				}
 				
 				remedyScore2[i]=score2[i];
-				
 			}
 		
 		}
@@ -257,18 +308,17 @@ void Data::calcScores2(int,string name[],float score2[],float total2,float avera
 		
 	cout<<endl;
 }
-
 //======================================================================================================//
 
 void Data::countAv(float score[],float score2[],float mean[],string name[])
 	{
 		for (int i=0 ; i<students ; i++) 
 		{
-			mean[i]=(score[i]+score2[i])/2;
+			mean[i]=(score[i]+score2[i])/2; 
+			//to calculate the average of the 1st and 2nd test
 		
 			cout<<name[i] <<"'s average test score = " << mean[i]<<endl;
 		}
-		cout<<endl<<endl;
 
 	}
 
@@ -280,29 +330,36 @@ void Data::midTerm(int,string name[],float score3[],float total3,float average3,
 	
 	for (int i=0 ; i<students ; i++) // to know student's tests scores
 	{
-		cout << "input "<<name[i]<<"'s mid test score "  << " = ";
+		cout << "input "<<name[i]<<"'s mid test score (0-100) "  << " = ";
 		cin>>midScore[i];
 		
 		while(midScore[i]>100 || midScore[i]<0)
 		{
-			cout << "input "<<name[i]<<"'s test score (0-100) "  << " = ";
+			cout << "input "<<name[i]<<"'s mid test score (0-100) "  << " = ";
 			cin>>midScore[i];
 		}
 		
 		total3 = total3 + midScore[i] ;
 	}
 	
+	cout<<endl;
 	average3 = total3 / students;
 	cout<<"average score = "<< average3<<endl<<endl;
 	
 	if(average3 < minScore) 
 	{
-		cout<<"The average score is bellow the minimum score (75)"<<endl<<"please input students's retest scores"<<endl<<endl;
+		cout<<"The average score is bellow the minimum score (75)"<<endl
+			<<"please input students's retest scores"<<endl<<endl;
 		
 			for (int i=0 ; i<students ; i++) 
 			{
-				cout << "input "<<name[i]<<"'s retest's score = ";
-				cin>>retestScore3[i];
+				do
+				{
+					cout << "input "<<name[i]<<"'s retest's score (0-100) = ";
+					cin>>retestScore3[i];
+				}
+				
+				while(midScore[i]>100 || midScore[i]<0);
 				
 				if(retestScore3[i]<midScore[i])
 				retestScore3[i]=midScore[i];
@@ -319,15 +376,19 @@ void Data::midTerm(int,string name[],float score3[],float total3,float average3,
 
 //=============================================================================================//
 
-
 void Data :: reportScore(int,float finalMidScore[],float mean[],float midScore[],string name[])
+//to show all the scores that must be inserted to the report card
 {
+	cout<<endl;
+	cout<<"name\t"<<"Average Score\t"<<"Mid Score\t"<<"Score For Now"<<endl<<endl;
+
 	for (int i=0 ; i<students ; i++) 
 	{
 		finalMidScore [i]= 0.3*mean[i]+0.7*midScore[i] ; 
+		//to calculate final score for the report
 	
-		cout<<"name\t"<<"Average Score\t"<<"Mid Score\t"<<"Score For Now"<<endl<<endl;
-		cout<<name[i]<<"\t"<<mean[i]<<"\t"<<midScore[i]<<finalMidScore[i]<<endl;
+		
+		cout<<name[i]<<"\t"<<mean[i]<<"\t\t"<<midScore[i]<<"\t\t"<<finalMidScore[i]<<endl<<endl;
 	}
 }
 
--- final final/main.cpp
+++ final final/main.cpp
@@ -1,3 +1,8 @@
+//  main.cpp
+// Final Project
+//  Sefira Karina-2001586155-Batch 2020
+//  Copyright © 2016 Sefira Karina. All rights reserved.
+
 #include<iostream>
 #include <string>
 #include "finalproject.hpp"
@@ -5,57 +10,101 @@ using namespace std;
 
 int main()
 {
-	int humans ;
+	int humans,choice ;
 	int num=20;
 	string namee[num] ;
 	float score[num],average,remedyScore[num],retestScore[num],total;
 	float score2[num],average2,remedyScore2[num],retestScore2[num],total2,mean[num];
 	float score3[num],average3,retestScore3[num],total3,midScore[num],finalMidScore[num];
 	
+	cout<< "WELCOME TO THE MID REPORT CARD SIMULATOR\n"
+		<<"-----------------------------------------"<<endl<<endl ;
+	
+do 
+//the menu will reappeared after each choice (except 3)
+{
+	
+	cout <<"What do you want to do?"<<endl<<endl;
+	cout<<"1.View scoring rules\n"<<"2.Start scoring\n"<<"3.Exit"<<endl<<endl;
+	cout<<"Input your choice (1-3) : " ; cin>>choice ; cout<<endl;
+
 	Data input;
 	
-	cout << "Enter number of students (max 20) = ";
-	cin >> humans ;
-	
-	while(humans > num)
+	if (choice==1)
 	{
+		input.rules();	
+		//call fucntion to show the rules of scoring)
+	}
+	
+	else if(choice==2)
+	{
+	
 		cout << "Enter number of students (max 20) = ";
-		cin >> humans ;		
-	}
+		//input the number of students 
+		cin >> humans ;
 	
-	cout<<endl;
+		while(humans > num)
+		/*input validation,if the student input is above 20,
+		the program will ask for reinput*/
+		{
+			cout << "Enter number of students (max 20) = ";
+			cin >> humans ;		
+		}
 	
-	input.setStudents(humans);
+		cout<<endl;
 	
-	for (int i=0 ; i<input.getStudents() ; i++) // to know students's names
-	{
-		cout << "Enter student "<<i+1<<"'s name = " ;
-		cin >> namee[i] ;
+		input.setStudents(humans);
+		//call mutator to set the amount of students
+	
+		for (int i=0 ; i<input.getStudents() ; i++) 
+		// to know students's names
+		{
+			cout << "Enter student "<<i+1<<"'s name = " ;
+			cin >> namee[i] ;
 		
-		input.setName(namee[i]);
-	}
-	cout<<endl ;
+			input.setName(namee[i]);
+		}
+		cout<<endl ;
 	
-	cout <<"1st test"<<endl ;
-	cout<<"------------"<<endl ;
-	input.calcScores(input.getStudents(),namee,score,total,average,retestScore,remedyScore);
+		cout <<"1st test"<<endl ;
+		cout<<"------------"<<endl ;
+		//call the function to input & calculate 1st test's scores
+		input.calcScores(input.getStudents(),namee,score,total,average,retestScore,remedyScore);
+		
+		cout<<"==============================================================================="<<endl;
 	
-	cout<<"==============================================================================="<<endl;
+		cout <<"2nd test"<<endl ;
+		cout<<"------------"<<endl ;
+		//call function to input & calculate 2nd test's scores
+		input.calcScores2(input.getStudents(),namee,score2,total2,average2,retestScore2,remedyScore2);
 	
-	cout <<"2nd test"<<endl ;
-	cout<<"------------"<<endl ;
-	input.calcScores2(input.getStudents(),namee,score2,total2,average2,retestScore2,remedyScore2);
+		cout<<"==============================================================================="<<endl;
+		//call function to calculate the average test scores
+		input.countAv(score,score2,mean,namee);
 	
-	cout<<"==============================================================================="<<endl;
-	
-	input.countAv(score,score2,mean,namee);
-	
-	cout<<"==============================================================================="<<endl;
-	
-	input.midTerm(input.getStudents(),namee,score3,total3,average3,retestScore3,midScore);
-	
-	input.reportScore(input.getStudents(),finalMidScore,mean,midScore, namee);
+		cout<<"==============================================================================="<<endl;
+		//call function to input & calculate mid test's scores
+		input.midTerm(input.getStudents(),namee,score3,total3,average3,retestScore3,midScore);
+		
+		//call function to show the final scores for the mid repord card
+		input.reportScore(input.getStudents(),finalMidScore,mean,midScore, namee);
 
-return 0 ;
+	}
+	
+	else if(choice==3)
+	//to exit the program,user must choose 3 in the menu
+	{
+		cout<<"OKAY,BYE!!" ;
+		return 0; 
+	}
+	
+	else
+	cout<<"PLEASE CHOSE FROM 1-3 ONLY\n\n";
+	
+}
+
+while(choice>0 || choice<4);
+	
+	return 0;
 }
 
